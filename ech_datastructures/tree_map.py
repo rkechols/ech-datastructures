18a74cb7867bff1bdf16502c4f8aef8f83f4de4e
Ryan Echols <rkechols21@gmail.com>
2022-05-07 17:43:29 -0600

    start of a TreeMap

--- ech_datastructures/tree_map.py
+++ ech_datastructures/tree_map.py
@@ -1,33 +1,57 @@
-from typing import Any, Generic, Iterable, Tuple, TypeVar, Union, Mapping
+from typing import Any, Generic, Iterable, List, Mapping, Tuple, TypeVar, Union
+
+from .avl_tree import AVLTree
 
 
 K = TypeVar("K")
 V = TypeVar("V")
 
 
+class _TreeMapNode(Generic[K, V]):
+    def __init__(self, k: K, v: V):
+        self.k = k
+        self.v = v
+
+    @staticmethod
+    def key(node: "_TreeMapNode[K, V]") -> K:
+        return node.k
+
+
 class TreeMap(Generic[K, V]):
+    __slots__ = "_tree",  # comma makes it a tuple, like it needs to be
+
     def __init__(self):
-        pass
+        self._tree: AVLTree[_TreeMapNode[K, V]] = AVLTree(key=_TreeMapNode.key)
 
     def clear(self):
-        # TODO
-        raise NotImplementedError
+        self._tree.clear()
 
     def get(self, k: K, default: V = None) -> V:
-        # TODO
-        raise NotImplementedError
+        result = self._tree.find_by_key(k)
+        if result is None:
+            if default is None:
+                raise KeyError(k)
+            else:
+                return default
+        return result.v
 
-    def items(self) -> Iterable[Tuple[K, V]]:
-        # TODO
-        raise NotImplementedError
+    def items(self) -> List[Tuple[K, V]]:
+        to_return = []
+        for node in self._tree:
+            to_return.append((node.k, node.v))
+        return to_return
 
-    def keys(self) -> Iterable[K]:
-        # TODO
-        raise NotImplementedError
+    def keys(self) -> List[K]:
+        to_return = []
+        for node in self._tree:
+            to_return.append(node.k)
+        return to_return
 
-    def values(self) -> Iterable[V]:
-        # TODO
-        raise NotImplementedError
+    def values(self) -> List[V]:
+        to_return = []
+        for node in self._tree:
+            to_return.append(node.v)
+        return to_return
 
     def pop(self, k: K, default: V = None) -> V:
         # TODO
@@ -64,8 +88,7 @@ class TreeMap(Generic[K, V]):
         raise NotImplementedError
 
     def __len__(self) -> int:
-        # TODO
-        raise NotImplementedError
+        return len(self._tree)
 
     def __ne__(self, other: Any) -> bool:
         # TODO
