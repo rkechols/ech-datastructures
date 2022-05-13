d81beba4baa42213189aebe110b84e690c104d2a
Ryan Echols <rkechols21@gmail.com>
2022-05-13 02:00:44 -0600

    a bunch of ops that need tests

--- ech_datastructures/tree_set.py
+++ ech_datastructures/tree_set.py
@@ -1,4 +1,5 @@
 from typing import Any, Generator, Generic, Iterable, TypeVar
+from itertools import chain
 
 from ech_datastructures.tree_map import TreeMap
 
@@ -8,6 +9,7 @@ T = TypeVar("T")
 
 def _subset_check(subset_candidate: Iterable[T], superset_candidate: Iterable[T]) -> bool:
     # by contract, both `self` and `other` are sorted and non-empty
+    # (but it's ok if they have duplicates)
     iter_other = iter(superset_candidate)
     curr_other = next(iter_other)
     for curr_self in subset_candidate:
@@ -29,6 +31,8 @@ def _subset_check(subset_candidate: Iterable[T], superset_candidate: Iterable[T]
 
 
 class TreeSet(Generic[T]):
+    __slots__ = ("_map",)
+
     def __init__(self, data: Iterable[T] = None):
         self._map: TreeMap[T, None] = TreeMap()
         # add any starter data
@@ -124,24 +128,49 @@ class TreeSet(Generic[T]):
         return is_superset and len(other) < self.__len__()
 
     def union(self, *others: Iterable[Iterable[T]]) -> "TreeSet[T]":
-        # TODO
-        raise NotImplementedError
+        to_return = self.copy()
+        to_return.update(others)
+        return to_return
 
     def __or__(self, other: "TreeSet[T]") -> "TreeSet[T]":
         if not isinstance(other, TreeSet):
             raise ValueError(f"`other` must be a TreeSet, but was {other.__class__.__name__}")
-        # TODO
-        raise NotImplementedError
+        to_return = self.copy()
+        to_return.update(other)
+        return to_return
 
     def intersection(self, *others: Iterable[Iterable[T]]) -> "TreeSet[T]":
-        # TODO
-        raise NotImplementedError
+        to_return = TreeSet()
+        # if self is empty, we can shortcut out
+        if self.__len__() == 0:
+            return to_return
+        # check each of the elems in `others` to see if they're in `self`
+        for other in others:
+            for elem in other:
+                if elem in self:
+                    to_return.add(elem)
+        return to_return
 
     def __and__(self, other: "TreeSet[T]") -> "TreeSet[T]":
         if not isinstance(other, TreeSet):
             raise ValueError(f"`other` must be a TreeSet, but was {other.__class__.__name__}")
-        # TODO
-        raise NotImplementedError
+        to_return = TreeSet()
+        # if either is empty, we can shortcut out
+        n_self = self.__len__()
+        if n_self == 0:
+            return to_return
+        n_other = len(other)
+        if n_other == 0:
+            return to_return
+        # iterate over the shorter one
+        if n_self < n_other:
+            set_iter, set_check = self, other
+        else:
+            set_iter, set_check = other, self
+        for elem in set_iter:
+            if elem in set_check:
+                to_return.add(elem)
+        return to_return
 
     def difference(self, *others: Iterable[Iterable[T]]) -> "TreeSet[T]":
         # TODO
@@ -164,21 +193,26 @@ class TreeSet(Generic[T]):
         raise NotImplementedError
 
     def copy(self) -> "TreeSet[T]":
-        # TODO
-        raise NotImplementedError
+        to_return = TreeSet(self)
+        return to_return
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, TreeSet):
             return False
-        # TODO
-        raise NotImplementedError
+        if self.__len__() != len(other):
+            return False
+        for self_elem, other_elem in zip(self, other):
+            if self_elem != other_elem:
+                return False
+        return True
 
     def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
     def update(self, *others: Iterable[Iterable[T]]):
-        # TODO
-        raise NotImplementedError
+        for other in others:
+            for elem in other:
+                self._map[elem] = None
 
     def __ior__(self, other: "TreeSet[T]"):
         if not isinstance(other, TreeSet):
@@ -217,23 +251,30 @@ class TreeSet(Generic[T]):
         raise NotImplementedError
 
     def add(self, elem: T):
-        # TODO
-        raise NotImplementedError
+        self._map[elem] = None
 
     def remove(self, elem: T):
-        # TODO
-        raise NotImplementedError
+        try:
+            del self._map[elem]
+        except KeyError:
+            # pylint: disable=raise-missing-from
+            raise KeyError(elem)
 
     def discard(self, elem: T):
-        # TODO
-        raise NotImplementedError
+        try:
+            del self._map[elem]
+        except KeyError:
+            pass  # literally do nothing
 
     def pop(self) -> T:
-        # TODO
-        raise NotImplementedError
+        try:
+            to_return, _ = self._map.popitem()
+        except KeyError:
+            # pylint: disable=raise-missing-from
+            raise KeyError("set is empty")
+        return to_return
 
     def clear(self):
-        # TODO
-        raise NotImplementedError
+        self._map.clear()
 
     __hash__ = None
